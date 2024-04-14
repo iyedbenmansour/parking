fba5441cba8be573a162bf89e307c7d8cf79cff9
iyedbenmansour <tniyed@gmail.com>
2024-04-14 14:26:43 +0100

    foooter fix

--- client/src/components/footer/Footer.jsx
+++ client/src/components/footer/Footer.jsx
@@ -7,18 +7,18 @@ const Footer = () => {
       <div className="container">
         <div className="row">
           <div className="footer-col">
-            <h4>company</h4>
+            <h4>OACA</h4>
             <ul>
-              <li><a href="#">about us</a></li>
-              <li><a href="#">our services</a></li>
-              <li><a href="#">privacy policy</a></li>
-              <li><a href="#">affiliate program</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/oaca">about us</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate">our services</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/conformité">privacy policy</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/gouvernance">gouvernance </a></li>
             </ul>
           </div>
           <div className="footer-col">
             <h4>get help</h4>
             <ul>
-              <li><a href="#">FAQ</a></li>
+              <li><a href="http://localhost:3000/help">FAQ</a></li>
               <li><a href="#">order status</a></li>
               <li><a href="#">payment options</a></li>
             </ul>
@@ -26,10 +26,9 @@ const Footer = () => {
           <div className="footer-col">
             <h4>Utils</h4>
             <ul>
-              <li><a href="#">watch</a></li>
-              <li><a href="#">bag</a></li>
-              <li><a href="#">shoes</a></li>
-              <li><a href="#">dress</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/accès-à-l-information">Accès</a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/relation-avec-le-citoyen">Client service </a></li>
+              <li><a href="https://www.oaca.nat.tn/web/corporate/marche">Job offers</a></li>
             </ul>
           </div>
           <div className="footer-col">
